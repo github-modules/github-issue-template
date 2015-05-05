c0bd71be8ce3982d72ac6cfe43846e4ee40aa988
Zeke Sikelianos <zeke@sikelianos.com>
2015-05-05 14:51:06 -0700

    one point oh

---
+++ example.sh
@@ -0,0 +1,1 @@
+github-issue-template --repo sally/project --title new thing --body path/to/body.md
---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const open = require('open')
+const url = require('url')
+const isUrl = require('is-url')
+const argv = require('yargs').argv
+
+if (argv.repo && argv.title && argv.body) {
+
+  console.log(path.join(process.cwd(), argv.body))
+
+  var issueURL = url.format({
+    protocol: 'https',
+    host: 'github.com',
+    pathname: `${argv.repo}/issues/new`,
+    query: {
+      title: argv.title,
+      body: fs.readFileSync(path.join(process.cwd(), argv.body), 'utf8')
+    }
+  })
+  process.stdout.write(issueURL)
+  if (isUrl(issueURL)){
+    open(issueURL)
+  }
+  process.exit()
+}
+
+console.log(`
+Usage: github-issue-template --repo sally/project --title new thing --body path/to/body.md`
+)
